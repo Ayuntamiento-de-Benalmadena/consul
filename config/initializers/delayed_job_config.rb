0ac88654fbfe1984b6a651bb5f95974ee834b401
Diego CC <diego.calvo@enreda.coop>
2022-10-05 17:54:43 +0200

    test reconnect after pg connection lost in DJ

--- config/initializers/delayed_job_config.rb
+++ config/initializers/delayed_job_config.rb
@@ -14,3 +14,10 @@ Delayed::Worker.read_ahead = 10
 Delayed::Worker.default_queue_name = "default"
 Delayed::Worker.raise_signal_exceptions = :term
 Delayed::Worker.logger = Logger.new(File.join(Rails.root, "log", "delayed_job.log"))
+
+Delayed::Worker.lifecycle.before :perform do
+  puts 'TEST!'
+  if !ActiveRecord::Base.connection.active?
+    establish_connection ActiveRecord::Base.configurations["#{Rails.env}"]
+  end
+end
