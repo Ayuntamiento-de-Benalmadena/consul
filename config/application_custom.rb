9a7a27f62a6a64505b8b82a69662248d844412f6
Diego CC <diego.calvo@enreda.coop>
2022-10-13 13:39:31 +0200

    languages only spanish and english and no comments in polls

--- config/application_custom.rb
+++ config/application_custom.rb
@@ -1,5 +1,7 @@
 module Consul
   class Application < Rails::Application
     config.i18n.default_locale = :es
+    available_locales = ["es", "en"]
+    config.i18n.available_locales = available_locales
   end
 end
